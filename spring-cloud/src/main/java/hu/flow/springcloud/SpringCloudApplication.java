eca469ff6917d051045f01d15bd8903620ad2dbe
Tamas Vajgely <vajgi90@gmail.com>
2020-04-07 10:17:14 +0200

    Configure class path for config

--- spring-cloud/src/main/java/hu/flow/springcloud/SpringCloudApplication.java
+++ spring-cloud/src/main/java/hu/flow/springcloud/SpringCloudApplication.java
@@ -2,8 +2,10 @@ package hu.flow.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudApplication {
 
 	public static void main(String[] args) {
