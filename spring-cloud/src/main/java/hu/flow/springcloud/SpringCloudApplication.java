7ea1b0c00d1f2a3c3e1f9201c0427a47d941910d
Tamas Vajgely <vajgi90@gmail.com>
2020-04-07 10:07:50 +0200

    Spring initialization

---
+++ spring-cloud/src/main/java/hu/flow/springcloud/SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package hu.flow.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApplication.class, args);
+	}
+
+}
